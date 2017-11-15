426d994e34f3408195ebc97c82b68ea2d1ae468a
Alp Eren Can <alperenc@gmail.com>
2017-11-15 14:02:54 +0300

    Refactor meals from mapStateToProps return

--- src/components/App.js
+++ src/components/App.js
@@ -18,13 +18,7 @@ const mapStateToProps = (calendar) => {
   return {
     calendar: dayOrder.map(day => ({
       day,
-      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
-        meals[meal] = calendar[day][meal]
-          ? calendar[day][meal] :
-          null
-
-        return meals
-      }, {})
+      meals: Object.assign(calendar[day])
     }))
   }
 }
