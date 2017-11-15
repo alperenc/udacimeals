d4649f07fd3ee74727c4eb64cdac2fbad7c10f24
Alp Eren Can <alperenc@gmail.com>
2017-11-15 15:53:37 +0300

    Add food reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -66,4 +66,19 @@ const calendar = (state = initialCalendarState, action) => {
   }
 }
 
+const food = (state = {}, action) => {
+  const { recipe } = action
+
+  switch (action.type) {
+    case ADD_RECIPE:
+      return {
+        ...state,
+        [recipe.label]: recipe
+      }
+
+    default:
+      return state
+  }
+}
+
 export default calendar
