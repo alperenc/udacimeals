d208ac5f7637a182d830b0c3864c5b4ce5f9d5d6
Alp Eren Can <alperenc@gmail.com>
2017-11-15 14:01:47 +0300

    Use connect()

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
 import reducer from './reducers'
 
 const store = createStore(
@@ -15,5 +15,5 @@ const store = createStore(
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>, document.getElementById('root'));
-registerServiceWorker();
+    </Provider>, document.getElementById('root'))
+registerServiceWorker()
