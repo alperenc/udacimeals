8fef38316cfa5fb00ea00a7a5017f1d557d25226
Alp Eren Can <alperenc@gmail.com>
2017-11-19 16:48:32 +0300

    Use a custom logger as middleware to print the store’s state to the console

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import reducer from './reducers'
 
+const logger = store => next => action => {
+    console.group(action.type)
+    console.info('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd(action.type)
+    return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(logger)
+    )
 )
 
 ReactDOM.render(
